a535857aaa293e1a6621eccdefad0b4e7e13fc9a
Nathaniel Bibler <git@nathanielbibler.com>
2011-05-03 22:44:37 -0400

    Create spec/support to hold custom spec modules

---
+++ spec/support/configuration.rb
@@ -0,0 +1,30 @@
+module ConfigurationHelper
+  def configure(&block)
+    HireFire.configure(&block)
+  end
+  
+  def with_configuration(&block)
+    old_configuration = HireFire.configuration
+    HireFire.configuration = HireFire::Configuration.new
+    yield(HireFire.configuration)
+    HireFire.configuration = old_configuration
+  end
+  
+  def with_max_workers(workers, &block)
+    with_configuration do |config|
+      config.max_workers = workers
+      yield
+    end
+  end
+  
+  def with_min_workers(workers, &block)
+    with_configuration do |config|
+      config.min_workers = workers
+      yield
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include ConfigurationHelper
+end
